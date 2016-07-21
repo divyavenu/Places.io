9d99009e8346ebf489b489d0a508f1b089c8c2a4
Baby's <acdevlin42@gmail.com>
2016-07-20 22:00:11 -0700

    Hardcode example image provided by Chris into map

--- static/map.js
+++ static/map.js
@@ -2,7 +2,7 @@ function initMap() {
   
   var map = new google.maps.Map(document.getElementById('map'), {
     // Map centers on the the Carnegie Mellon Silicon Valley Campus
-    zoom: 2,
+    zoom: 3,
     center: {lat: 37.4090697, lng: -122.06382530000002},
 
     // Adds option to switch between map and satellite imagery
@@ -32,8 +32,8 @@ function initMap() {
 
   function placeMarkerWithInfoWindow(lat, lng, title, infotitle, infoimageurl) {
     // Place a marker with title "infotitle" that renders image at url "infoimageurl"
-    var contentString = '<h1>{infotitle}</h1><br>' +
-                        '<img src="{infoimageurl}" alt="Pretty Pictures!!" style="width:300px;height:220px;">';
+    var contentString = `<h1>${infotitle}</h1><br>` +
+                        `<img src="${infoimageurl}" alt="Pretty Pictures!!" style="width:300px;height:220px;">`;
 
     var infowindow = new google.maps.InfoWindow({
       content: contentString
@@ -49,18 +49,26 @@ function initMap() {
       infowindow.open(map, marker);
     });
   }
-  // Example code - auto generates one marker on the map every 2 seconds 
-  // until 20 markers have been generated
-  
-  var count = 0;
-  var interval = setInterval(function() {
-    placeMarker(Math.random() * 100, Math.random()*-100, 'ANOTHER MARKER');
-    count++;
-    if (count > 20) {
-      clearInterval(interval);
-    }
-  }, 2000);
+// Example code - auto generates one marker on the map every 2 seconds 
+// until 20 markers have been generated
+/*
+var count = 0;
+var interval = setInterval(function() {
+  placeMarker(Math.random() * 100, Math.random()*-100, 'ANOTHER MARKER');
+  count++;
+  if (count > 20) {
+    clearInterval(interval);
+  }
+}, 2000);
+*/
 
+  // TODO XXX hardcoded example for 7/21 presentation, remove this and use api call instead
+  var infourl = 'https://lh3.googleusercontent.com/-Lvy0wxH9-AM/V4wCxtbebMI/AAAAAAAAAAs/wHsJ0Qdu6lQQs-rR-2lywFqxl5Flsjo3gCHM/IMG_4445.jpg';
+  var latitude = 39.9037777;
+  var longitude = 116.3918916;
+  var infotitle = 'IMG_4445.jpg';
+  var title = "IMAGE 1";
+  placeMarkerWithInfoWindow(latitude, longitude, title, infotitle, infourl);
 }
 
 // Adds a script making a call to the google maps API with the appropriate API key
