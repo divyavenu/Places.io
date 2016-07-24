1b7f9d3d3ebdbc38b60d4ea3169b8a97055c4cde
divyavenu <divya1993@gmail.com>
2016-07-24 13:47:54 -0700

    Saving photo meta data from Picasa to database

--- application/models.py
+++ application/models.py
@@ -1,13 +1,23 @@
 from application import db
 
-
-class Data(db.Model):
+class Photo(db.Model):
 	id = db.Column(db.Integer,primary_key=True)
+	useremail = db.Column(db.String(30))
+	title = db.Column(db.String(75))
+	infotitle = db.Column(db.String(75))
+	infourl = db.Column(db.String(1600))
 	latitute = db.Column(db.Numeric)
 	longitute = db.Column(db.Numeric)
-	date = db.Column(db.DateTime)
+	timestamp = db.Column(db.String(50))
+	epochtime = db.Column(db.Float)
 
-	def __init__(self,latitute,longitute,date):
+	def __init__(self,useremail,title,infotitle,infourl,latitute,longitute,timestamp,epochtime):
+		self.useremail = useremail
+		self.title = title
+		self.infotitle = infotitle
+		self.infourl = infourl
 		self.latitute = latitute
 		self.longitute = longitute
-		self.date = date
+		self.timestamp = timestamp
+		self.epochtime = epochtime
+
